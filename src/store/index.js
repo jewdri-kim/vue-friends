4f6695aaf77a9ac3a66ec9f90aa81b6250ec968f
jewdri-kim <ksoldier86@naver.com>
2021-04-17 14:59:43 +0900

    d

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,46 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state: {
+    todoList: [
+      { /*
+        date: Date,
+        do : String,
+        isEnd : Boolean
+        id: Number */
+      }
+    ],
+    toDayDate : null,
+    time : null,
+    todoChkNum : null
   },
   mutations: {
+    addToDoItem(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoList.push(todoItem);
+     },
+    deleteToDoItem(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoList.splice(index,1);
+    },
+    clearToToList(){
+      localStorage.clear();
+      this.todoList = [];
+    },
+    completedToDo(todoItem, index){
+      this.todoList[index].isEnd = !this.todoList[index].isEnd;
+    }
   },
   actions: {
+    addToDoItem(context, todoItem){
+      context.commit('addToDoItem',todoItem);
+    }
   },
   modules: {
+  },
+  getters:{
+    toDayDate (){},
+    time(){}
   }
 })
